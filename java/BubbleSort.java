05dd9859f4cb9109d2880be49883e01e6ca052ae
deeplisinha <deepalisinha94@gmail.com>
2018-12-27 20:01:45 +0530

    adding bubblesort implementation:java

---
+++ java/BubbleSort.java
@@ -0,0 +1,19 @@
+public class BubbleSort{
+
+	public static void main(String[]args) {
+		int[]arr= {2,8,9,6,4,5,1,3};
+		for(int i=arr.length-1;i>1;i--) {
+			for(int j=0;j<i;j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		for(int k=0;k<arr.length;k++) {
+			System.out.println(arr[k]);
+		}
+	
+	}
+}
